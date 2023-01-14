1c1023dd5ba6b8c14a60e8dbdaf73543d595a8fe
Angry-Doggaebi <120698922+Angry-Doggaebi@users.noreply.github.com>
2023-01-14 18:32:20 +0900

    Add files via upload

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,9 +1,15 @@
 function checkEmail(self){
     var useremail = document.getElementById('email').value;
+    var error = document.getElementById('error');
+
     let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
     if (regex.test(useremail)==false){
       document.getElementById('notice').innerHTML = 'Please provide a valid email';
+      
+      error.style.opacity = '1';
     } else {
       document.getElementById('notice').innerHTML = 'Good';
+
+      error.style.opacity = '0';
     }
 }
