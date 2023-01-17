2189b2c9135c59aae019c7bd2d603a29f4e379c2
Angry-Doggaebi <120698922+Angry-Doggaebi@users.noreply.github.com>
2023-01-17 16:57:25 +0900

    State change on click

--- notifications-page-main/script.js
+++ notifications-page-main/script.js
@@ -1,3 +1,4 @@
+//모든 슬라이드가 함께 작동
 // $(document).ready( function() {
 //     $( '.person' ).click( function() {
 //         $('.messages').slideToggle(200);
@@ -5,13 +6,20 @@
 // });
 
 
+//선택한 슬라이드만 작동
+// $(document).ready(function(){
+//     $(".notifications > .person").click(function(){
+//         $(this).children(".messages").slideToggle(200);
+//     });
+// });
+
+//선택한 슬라이드, 배경색, 리드마크 작동
 $(document).ready(function(){
     $(".notifications > .person").click(function(){
         $(this).children(".messages").slideToggle(200);
+        $(this).css("backgroundColor", "white");
+        // $(this).children(".stories").children(".name_detail").children(".readmark").css("display", "none");
+        $(this).find(".readmark").css("display", "none");
     });
 });
-
-
-
-
 
