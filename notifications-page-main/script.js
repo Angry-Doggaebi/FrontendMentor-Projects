27031eaf268558d1e9c6fb408040db3504455095
Angry-Doggaebi <120698922+Angry-Doggaebi@users.noreply.github.com>
2023-01-21 11:20:18 +0900

    Add files via upload

--- notifications-page-main/script.js
+++ notifications-page-main/script.js
@@ -13,13 +13,18 @@
 //     });
 // });
 
-//선택한 슬라이드, 배경색, 리드마크 작동
+
+
+//선택한 슬라이드, 배경색, 리드마크, 링크된 페이지 작동
 $(document).ready(function(){
     $(".notifications > .person").click(function(){
         $(this).children(".messages").slideToggle(200);
         $(this).css("backgroundColor", "white");
         // $(this).children(".stories").children(".name_detail").children(".readmark").css("display", "none");
         $(this).find(".readmark").css("display", "none");
+    });
+    $(".noti_link").not(".person").click(function(){
+        $(this).css("color", "hsl(219, 12%, 42%)");
     });
 });
 
