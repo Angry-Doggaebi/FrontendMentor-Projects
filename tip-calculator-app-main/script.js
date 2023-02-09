f2122e34041dcd6c48d92af181879f6a6b155fcd
AngryDoggaebi <120698922+AngryDoggaebi@users.noreply.github.com>
2023-02-09 10:56:20 +0900

    Refinement of calculation function and design fix

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,12 +1,22 @@
-//클릭한 팁 퍼센테이지를 전역변수로 활용
+//클릭한 팁 버튼 또는 작성한custom의 값을 전역변수에 저장
+//사람 수 또는 bill 값이 팁 금액보다 나중에 설정되는 경우 대비
 var tipPercent;
 
-//Bill만 입력된 경우
-//입력한 bill값을 실시간으로 읽어와 total 부분에 반영
+//Bill
 $(document).ready(function(){
+    //입력되는 값 실시간으로 가져오기
     $("#billNum").on("propertychange change keyup paste input", function() {
         var billNewValue = this.value;
+        let check = /^[0-9.]+$/ //숫자체크
+
+        document.getElementById('tipAmount').innerHTML = '$0.00';
         document.getElementById('total').innerHTML = '$'+billNewValue;
+
+        //값이 숫자가 아닌 경우 0.00 출력
+        //박스에 숫자를 썼다 지우는 경우 total이 빈 칸으로 출력되는 경우 대비
+        if (!check.test(billNewValue)){
+            document.getElementById('total').innerHTML = '$0.00';
+        };
     });
 });
 
@@ -46,21 +56,26 @@ function btnClickEvent(tipPercentage){
     tipPercent = tipPercentage; 
 
     calculationDependingOnPeopleNum(tipPercentage);
+
 };
 
 //custom
 $(document).ready(function(){
-    //Select Tip 값 실시간으로 가져오기
-    $("#btnNum").on("propertychange change keyup paste input", function() {
+    $("#custom").on("propertychange change keyup paste input", function() {
         //입력한 tip 값을 백분율에서 소수로 변환
         var tipPercentage = this.value * 0.01;
+        //전역변수 값 변경
+        tipPercent = tipPercentage; 
 
         calculationDependingOnPeopleNum(tipPercentage);
     });
 });
+// function customClickEvent(){
+//     
+// };
 
-//Number of people 값 실시간으로 가져오기
-//버튼에서 입력받아 전역변수로 바꾸어놓은 변수 tipPercent의 값 사용
+//Number of people
+//button, custom 값 입력받아 전역변수로 바꾸어놓은 변수 tipPercent 사용 (사람 수를 팁 금액보다 나중에 선택하는 경우에도 작동하도록)
 $(document).ready(function(){
     $("#peopleNum").on("propertychange change keyup paste input", function() {
 
@@ -69,10 +84,9 @@ $(document).ready(function(){
         // 입력한 bill값 
         var billNewValue = document.getElementById('billNum').value;
 
-           
         if (tipPercent == null){ //버튼을 안 눌렀을 경우  
             document.getElementById('total').innerHTML = '$'+billNewValue/peopleNewValue;
-            document.getElementById('tipAmount').innerHTML = '$'+0;
+            document.getElementById('tipAmount').innerHTML = '$'+0.00;
         } else { //버튼 누른 경우
             //tip amount 계산 후 변수에 입력 및 출력
             var tipAmount = billNewValue/peopleNewValue*tipPercent;
@@ -82,6 +96,14 @@ $(document).ready(function(){
             //total 계산 후 변수에 입력, '$'문자열과 함께 출력
             var total =billNewValue/peopleNewValue+tipAmount;
             document.getElementById('total').innerHTML = '$'+total.toFixed(2);
-        }
+        }; 
     });
 });
+
+//reset
+function resetEvent(){
+    var tipAmount = document.getElementById('tipAmount');
+    var total = document.getElementById('total');
+    tipAmount.innerHTML = '$0.00';
+    total.innerHTML = '$0.00';
+};
